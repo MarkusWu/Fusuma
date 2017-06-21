df6854d5d0fec1e2cc7172e6f31c5132d50bd730
MarkusWu <wu_markus@yahoo.com>
2017-06-21 12:27:33 -0400

    Rename property

--- Sources/FusumaViewController.swift
+++ Sources/FusumaViewController.swift
@@ -134,7 +134,7 @@ public class FusumaViewController: UIViewController {
         self.instance = nil
     }
     
-    public static var isSharedInstanceInitiated: Bool {
+    public static var sharedInstanceIsInitiated: Bool {
         return self.instance != nil
     }
     
